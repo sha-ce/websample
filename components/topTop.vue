af6f46418490b85b53f0d43afda6dbfa2f0451b3
sha ce <82579992+sha-ce@users.noreply.github.com>
2021-09-11 21:56:53 +0900

    save

--- components/topTop.vue
+++ components/topTop.vue
@@ -87,7 +87,7 @@
   padding: min(20px, 2vw) min(40px, 3.8vw);
   text-decoration: none;
   color: gray;
-  border: solid min(2px, 0.1vw) gray;
+  border: min(2px, 0.1vw) solid gray;
   border-radius: 50px;
   display: inline;
   white-space: nowrap;
