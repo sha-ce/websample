80a6f61488d6af007370e88fe3e8207c7ba35f15
sha ce <82579992+sha-ce@users.noreply.github.com>
2021-09-13 20:29:52 +0900

    save

--- pages/index.vue
+++ pages/index.vue
@@ -5,6 +5,8 @@
     <TopTop/>
     <Carousel/>
     <Carouselhooper/>
+    <Main/>
+    <Footer/>
   </div>
 </template>
 
@@ -12,11 +14,13 @@
 import Menu from '../components/Menu.vue';
 import Header from '../components/Header.vue';
 import TopTop from '../components/topTop.vue';
+import Main from '../components/topTop.vue';
+import Footer from '../components/topTop.vue';
 import Carousel from '../components/Carousel-sample.vue';
 import Carouselhooper from '../components/Carousel-hooper.vue';
 
 export default {
-  components: { Header, Menu, TopTop, Carousel, Carouselhooper },
+  components: { Header, Menu, TopTop, Main, Footer, Carousel, Carouselhooper },
 }
 </script>
 
